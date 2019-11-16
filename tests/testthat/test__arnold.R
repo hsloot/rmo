bc6673db324178b388b3518583347278575afad0
Henrik Sloot <Henrik.Sloot@gmail.com>
2019-11-16 18:46:43 +0100

    test: test more parameter configartions

--- tests/testthat/test__arnold.R
+++ tests/testthat/test__arnold.R
@@ -3,12 +3,34 @@ context("Arnold model")
 use_seed <- 1632L
 
 ## Test that the implementation Arnold model
-## works as expected for d = 2.
+## works as expected for d = 2 and different choices
+## for the intensity vector..
 
 test_that("Arnold model implementation for d = 2", {
-  n <- 100L
+  n <- 25L
 
-  args <- list("d" = 2L, "intensities" = c(0.5, 0.4, 0.2))
+  ## all equal
+  args <- list("d" = 2L, "intensities" = c(1, 1, 1))
+  expect_equal_sampling_result("rmo_arnold", "test__rmo_arnold_bivariate_R",
+                               args, n, use_seed)
+
+  ## exchangeable, low, high
+  args <- list("d" = 2L, "intensities" = c(0.1, 0.1, 2))
+  expect_equal_sampling_result("rmo_arnold", "test__rmo_arnold_bivariate_R",
+                               args, n, use_seed)
+
+  ## exchangeable, high, low
+  args <- list("d" = 2L, "intensities" = c(3, 3, 0.5))
+  expect_equal_sampling_result("rmo_arnold", "test__rmo_arnold_bivariate_R",
+                               args, n, use_seed)
+
+  ## Low, High, Low
+  args[["intensities"]] <- c(0.5, 3, 0.2)
+  expect_equal_sampling_result("rmo_arnold", "test__rmo_arnold_bivariate_R",
+                               args, n, use_seed)
+
+  ## Low, Low, High
+  args[["intensities"]] <- c(0.1, 0.005, 2)
   expect_equal_sampling_result("rmo_arnold", "test__rmo_arnold_bivariate_R",
                                args, n, use_seed)
 })
--- tests/testthat/test__ex_arnold.R
+++ tests/testthat/test__ex_arnold.R
@@ -3,11 +3,22 @@ use_seed <- 1632L
 
 ## Test that the implementation of the modified version of the
 ## Arnold model for exchangeable distributions works as intended
-## for d = 2.
+## for d = 2 and different choices for the ex_intensity vector.
 test_that("Exchangeable Arnold model for d = 2", {
-  n <- 100L
+  n <- 25L
 
-  args <- list("d" = 2L, ex_intensities = c(0.5, 1))
+  ## all equal
+  args <- list("d" = 2L, ex_intensities = c(1, 1))
+  expect_equal_sampling_result("rmo_ex_arnold", "test__rmo_ex_arnold_bivariate_R",
+                               args, n, use_seed)
+
+  ## low, high
+  args[["ex_intensities"]] <- c(0.5, 2)
+  expect_equal_sampling_result("rmo_ex_arnold", "test__rmo_ex_arnold_bivariate_R",
+                               args, n, use_seed)
+
+  ## high, low
+  args[["ex_intensities"]] <- c(3, 0.2)
   expect_equal_sampling_result("rmo_ex_arnold", "test__rmo_ex_arnold_bivariate_R",
                                args, n, use_seed)
 })
@@ -15,10 +26,17 @@ test_that("Exchangeable Arnold model for d = 2", {
 
 
 ## Test that alternativ eimplementation based on the `a`
-## parameters is equivalent for d = 5.
+## parameters is equivalent for d = 5 and different choices
+## for the ex_intensity vector..
 test_that("Alternative implementation", {
   n <- 100L
 
+  ## all equal
+  args <- list("d" = 5L, ex_intensities = rep(1, 5L))
+  expect_equal_sampling_result("rmo_ex_arnold", "test__rmo_ex_arnold_alternative_R",
+                               args, n, use_seed)
+
+  ## heterogeneous ex_intensity vector
   args <- list("d" = 5L, ex_intensities = c(0.4, 0.3, 0.2, 0.2, 0.1))
   expect_equal_sampling_result("rmo_ex_arnold", "test__rmo_ex_arnold_alternative_R",
                                args, n, use_seed)
