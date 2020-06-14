42e2e2efb901b4f991cf4b3b558981712aaee686
Henrik Sloot <henrik.sloot@gmail.com>
2020-06-15 00:04:24 +0200

    refactor-test: LFM
    - fixes #32
    - enrich test implementation with comments
    - remove bivariate implementations

--- tests/testthat/helper__lfm_cpp.R
+++ tests/testthat/helper__lfm_cpp.R
@@ -1,159 +1,15 @@
-#' Bivariate implementation of the Compound Poisson process LFM
-#'
-#' @rdname rmo_lfm_cpp
-#' @keywords internal
-#' @noRd
-test__rmo_lfm_cpp_bivariate_R <- function(n, d, rate, rate_killing, rate_drift, rjump_name, rjump_arg_list) { # nolint
-
-  if (rjump_name == "rexp") {
-    return(test__rmo_lfm_cpp_bivariate_rexp_R(n, rate, rate_killing,
-      rate_drift, rjump_arg_list$rate))
-  } else if (rjump_name == "rposval") {
-    return(test__rmo_lfm_cpp_bivariate_rposval_R(n, rate, rate_killing,
-      rate_drift, rjump_arg_list$value))
-  }
-}
-
-#' @rdname rmo_lfm_cpp
-#' @keywords internal
-#' @noRd
-test__rmo_lfm_cpp_bivariate_rexp_R <- function(n, rate, rate_killing, rate_drift, jump_rate) { # nolint
-
-  out <- matrix(NA, nrow=n, ncol=2L)
-  for (k in 1:n) {
-    unit_exponentials <- rexp(2L, 1)
-    if (rate_drift>0.) {
-      barrier_values <- sort(unit_exponentials)
-    } else {
-      barrier_values <- max(unit_exponentials)
-    }
-    d <- length(barrier_values)
-
-    killing_time <- rexp_(1, rate_killing)
-
-    times <- 0.
-    values <- 0.
-    for (i in 1:d) {
-      while (last(values) < barrier_values[[i]]) {
-        waiting_time <- rexp_(1, rate)
-        killing_waiting_time <- killing_time - last(times)
-        jump_value <- rexp_(1, jump_rate)
-
-        if (killing_waiting_time < Inf && killing_waiting_time <= waiting_time) {
-          for (j in i:d) {
-            if (rate_drift>0. &&
-                (barrier_values[[j]] - last(values))/rate_drift<=killing_waiting_time) {
-              intermediate_waiting_time <- (barrier_values[[j]] - last(values)) / rate_drift
-              times <- c(times, last(times) + intermediate_waiting_time)
-              values <- c(values, barrier_values[[j]])
-              killing_waiting_time <- killing_waiting_time - intermediate_waiting_time
-            }
-          }
-
-          times <- c(times, last(times) + killing_waiting_time)
-          values <- c(values, Inf)
-        } else {
-          for (j in i:d) {
-            if (rate_drift>0. && (barrier_values[[j]] - last(values))/rate_drift <= waiting_time) {
-              intermediate_waiting_time <- (barrier_values[[j]] - last(values)) / rate_drift
-              times <- c(times, last(times) + intermediate_waiting_time)
-              values <- c(values, barrier_values[[j]])
-              waiting_time <- waiting_time - intermediate_waiting_time
-            }
-          }
-
-          if (rate>0) { ## waiting_time<Inf # nolint
-            times <- c(times, last(times) + waiting_time)
-            values <- c(values, last(values) + waiting_time * rate_drift + jump_value)
-          }
-        }
-      }
-    }
-
-    cpp_subordinator <- cbind("t"=times, "values"=values)
-    out[k, ] <- vapply(1:2L, function(x) min(cpp_subordinator[cpp_subordinator[, 2] >= unit_exponentials[[x]], 1]), FUN.VALUE=0.5) # nolint
-  }
-
-  out
-}
-
-#' @rdname rmo_lfm_cpp
-#' @keywords internal
-#' @noRd
-test__rmo_lfm_cpp_bivariate_rposval_R <- function(n, rate, rate_killing, rate_drift, jump_value) { # nolint
-
-  out <- matrix(NA, nrow=n, ncol=2L)
-  for (k in 1:n) {
-    unit_exponentials <- rexp(2L, 1)
-    if (rate_drift>0.) {
-      barrier_values <- sort(unit_exponentials)
-    } else {
-      barrier_values <- max(unit_exponentials)
-    }
-    d <- length(barrier_values)
-
-    killing_time <- rexp_(1, rate_killing)
-
-    times <- 0.
-    values <- 0.
-    for (i in 1:d) {
-      while (last(values) < barrier_values[i]) {
-        waiting_time <- rexp_(1, rate)
-        killing_waiting_time <- killing_time - last(times)
-
-        if (killing_waiting_time < Inf && killing_waiting_time <= waiting_time) {
-          for (j in i:d) {
-            if (rate_drift>0. &&
-                (barrier_values[[j]] - last(values))/rate_drift<=killing_waiting_time) {
-              intermediate_waiting_time <- (barrier_values[[j]] - last(values)) / rate_drift
-              intermediate_value <- barrier_values[[j]]
-              times <- c(times, last(times) + intermediate_waiting_time)
-              values <- c(values, intermediate_value)
-              killing_waiting_time <- killing_waiting_time - intermediate_waiting_time
-            }
-          }
-
-          times <- c(times, last(times) + killing_waiting_time)
-          values <- c(values, Inf)
-        } else {
-          for (j in i:d) {
-            if (rate_drift>0. &&
-                (barrier_values[[j]] - last(values))/rate_drift <= waiting_time) {
-              intermediate_waiting_time <- (barrier_values[[j]] - last(values)) / rate_drift
-              intermediate_value <- barrier_values[[j]]
-              times <- c(times, last(times) + intermediate_waiting_time)
-              values <- c(values, intermediate_value)
-              waiting_time <- waiting_time - intermediate_waiting_time
-            }
-          }
-
-          if (rate>0.) { ## waiting_time<Inf # nolint
-            times <- c(times, last(times) + waiting_time)
-            values <- c(values, last(values) + waiting_time * rate_drift + jump_value)
-          }
-        }
-      }
-    }
-
-    cpp_subordinator <- cbind("t"=times, "values"=values)
-    out[k, ] <- vapply(1:2L, function(x) min(cpp_subordinator[cpp_subordinator[, 2] >= unit_exponentials[[x]], 1]), FUN.VALUE=0.5) # nolint
-  }
-
-  out
-}
-
-
-
-
-
 #' Alternative implementation of ind.-case via LFM
 #'
 #' @keywords internal
 #' @noRd
-test__rmo_lfm_cpp_independence_R <- function(n, d, rate, rate_killing, rate_drift, rjump_name, rjump_arg_list) { # nolint
-
+test__rmo_lfm_cpp_independence <- function(
+    n, d,
+    rate, rate_killing, rate_drift,
+    rjump_name, rjump_arg_list) {
   out <- matrix(NA, nrow=n, ncol=d)
   for (k in 1:n) {
+    ## sample barriers and the values when they are surpassed by
+    ## the drift
     unit_exponentials <- rexp(d, 1)
     out[k, ] <- unit_exponentials / rate_drift
   }
@@ -165,12 +21,16 @@ test__rmo_lfm_cpp_independence_R <- function(n, d, rate, rate_killing, rate_drif
 #'
 #' @keywords internal
 #' @noRd
-test__rmo_lfm_cpp_comonotone_R <- function(n, d, rate, rate_killing, rate_drift, rjump_name, rjump_arg_list) { # nolint
-
+test__rmo_lfm_cpp_comonotone <- function(
+    n, d,
+    rate, rate_killing, rate_drift,
+    rjump_name, rjump_arg_list) {
   out <- matrix(NA, nrow=n, ncol=d)
   for (k in 1:n) {
     stopifnot(all(rexp(d, 1) >= 0)) ## dummy
-    out[k, ] <- rexp(1L, rate_killing)
+    ## sample killing time
+    killing_time <- rexp(1L, rate_killing)
+    out[k, ] <- killing_time
   }
 
   out
@@ -182,16 +42,30 @@ test__rmo_lfm_cpp_comonotone_R <- function(n, d, rate, rate_killing, rate_drift,
 #' @rdname rmo_lfm_cpp
 #' @keywords internal
 #' @noRd
-test__rmo_lfm_cpp_R <- function(n, d, rate, rate_killing, rate_drift, rjump_name, rjump_arg_list = list()) { # nolint
-
+test__rmo_lfm_cpp <- function(
+    n, d,
+    rate, rate_killing, rate_drift,
+    rjump_name, rjump_arg_list = list()) {
   out <- matrix(NA, nrow=n, ncol=d)
   for (k in 1:n) {
+    ## sample barrier values
     unit_exponentials <- rexp(d)
-    cpp_subordinator <- test__sample_cpp_R(rate, rate_killing, rate_drift,
-      rjump_name, rjump_arg_list, unit_exponentials)
-    out[k, ] <- vapply(1:d, function(x) {
-      min(cpp_subordinator[cpp_subordinator[, 2] >= unit_exponentials[[x]], 1])
-    }, FUN.VALUE=0.5)
+    ## sample CPP path (return is array with <times, values>)
+    cpp_subordinator <- test__sample_cpp(
+      rate, rate_killing, rate_drift,
+      rjump_name, rjump_arg_list,
+      unit_exponentials)
+    ## find the smallest time-index value for which the
+    ## subordinator surpasses the respective barriers
+    times <- cpp_subordinator[, "t"]
+    values <- cpp_subordinator[, "value"]
+    out[k, ] <- vapply(
+      1:d,
+      function(x) {
+        min(times[values >= unit_exponentials[[x]]])
+      },
+      FUN.VALUE=0.5
+    )
   }
 
   out
@@ -200,12 +74,20 @@ test__rmo_lfm_cpp_R <- function(n, d, rate, rate_killing, rate_drift, rjump_name
 #' @rdname rmo_lfm_cpp
 #' @keywords internal
 #' @noRd
-test__sample_cpp_R <- function(rate, rate_killing, rate_drift, rjump_name, rjump_arg_list, barrier_values) { # nolint
+test__sample_cpp <- function(
+    rate, rate_killing, rate_drift,
+    rjump_name, rjump_arg_list,
+    barrier_values) {
+  ## get jump distribution
   if ("rexp" == rjump_name)
     rjump <- match.fun(stats::rexp)
   else if ("rposval" == rjump_name)
     rjump <- match.fun(rposval)
+  else
+    stop(sprintf("%s not implemented", rjump_name))
 
+  ## sort or maximise the barrier values (we only need to account for the
+  ## specific barrier values if a drift is present)
   if (rate_drift>0.) {
     barrier_values <- sort(barrier_values)
   } else {
@@ -213,21 +95,27 @@ test__sample_cpp_R <- function(rate, rate_killing, rate_drift, rjump_name, rjump
   }
   d <- length(barrier_values)
 
+  ## sample killing time
   killing_time <- rexp_(1, rate_killing)
 
   times <- 0.
   values <- 0.
   for (i in 1:d) {
     while (last(values) < barrier_values[[i]]) {
+      ## sample waiting time as well as jump value and reduce killing time
       waiting_time <- rexp_(1, rate)
       killing_waiting_time <- killing_time - last(times)
       jump_value <- do.call(rjump, args=c("n"=1, rjump_arg_list))
 
       if (killing_waiting_time < Inf && killing_waiting_time <= waiting_time) {
+        ## if killing happends before the next shock, calculate the time
+        ## where individual barriers are surpassed
         for (j in i:d) {
           if (rate_drift>0. &&
               (barrier_values[[j]] - last(values)) /
                 rate_drift<=killing_waiting_time) {
+            ## if barrier is surpassed by drift before killing set the
+            ## value accordingly
             intermediate_waiting_time <- (barrier_values[[j]] - last(values)) / rate_drift
             times <- c(times, last(times) + intermediate_waiting_time)
             values <- c(values, barrier_values[[j]])
@@ -241,6 +129,8 @@ test__sample_cpp_R <- function(rate, rate_killing, rate_drift, rjump_name, rjump
         for (j in i:d) {
           if (rate_drift>0. &&
               (barrier_values[[j]] - last(values))/rate_drift <= waiting_time) {
+            ## if killing does not happen before the next shock, but barrier is
+            ## surpassed by drift, set the value accordingly
             intermediate_waiting_time <- (barrier_values[[j]] - last(values)) / rate_drift
             times <- c(times, last(times) + intermediate_waiting_time)
             values <- c(values, barrier_values[[j]])
@@ -249,6 +139,8 @@ test__sample_cpp_R <- function(rate, rate_killing, rate_drift, rjump_name, rjump
         }
 
         if (rate>0.) { ## waiting_time<Inf # nolint
+          ## otherwise update the component values with the value of
+          ## the subordinator at time + waiting_time
           times <- c(times, last(times) + waiting_time)
           values <- c(values, last(values) + waiting_time * rate_drift + jump_value)
         }
--- tests/testthat/test__cpp.R
+++ tests/testthat/test__cpp.R
@@ -1,15 +1,10 @@
 context("Sample compound Poisson process")
 use_seed <- 1632L
 
-## #### Test implementation with exp. dist. jumps for the bivariate case ####
-#
-# Test that the implementation of the Compound Poisson process LFM with
-# exponentially distributed jumps works as expected for the bivariate case
-# and different choices of parameters.
-test_that("biv. CPP implementation works as intended for exp. jumps", {
+test_that("CPP implementation works as intended for exp. jumps", {
   set.seed(use_seed-543L)
-  d <- 2L
-  barrier_values <- rexp(2L)
+  d <- 5L
+  barrier_values <- rexp(d)
   set.seed(use_seed)
 
   ## rate = 0.5, rate_killing = 0, rate_drift = 0, jump_rate = 2
@@ -18,32 +13,29 @@ test_that("biv. CPP implementation works as intended for exp. jumps", {
     "rjump_name" = "rexp", "rjump_arg_list" = list("rate"=2),
     "barrier_values"=barrier_values)
   expect_equal_rn_generation(
-    "sample_cpp", "test__sample_cpp_R",
+    "sample_cpp", "test__sample_cpp",
     args, use_seed=use_seed)
 
   ## rate = 0.5, rate_killing = 0.2, rate_drift = 0, jump_rate = 2
   args[["rate_killing"]] <- 0.2
   expect_equal_rn_generation(
-    "sample_cpp", "test__sample_cpp_R",
+    "sample_cpp", "test__sample_cpp",
     args, use_seed=use_seed)
 
   ## rate = 0.5, rate_killing = 0.2, rate_drift = 0.1, jump_rate = 2
   args[["rate_drift"]] <- 0.1
   expect_equal_rn_generation(
-    "sample_cpp", "test__sample_cpp_R",
+    "sample_cpp", "test__sample_cpp",
     args, use_seed=use_seed)
 })
 
 
 ## #### Test implementation with det. jumps for the bivariate case ####
-#
-# Test that the implementation of the Compound Poisson process with
-# deterministic jumps works as expected for bivariate case and different
-# choices of parameters.
-test_that("biv. CPP implementation works as intended for det. jumps", {
+
+test_that("CPP implementation works as intended for det. jumps", {
   set.seed(use_seed-543L)
-  d <- 2L
-  barrier_values <- rexp(2L)
+  d <- 5L
+  barrier_values <- rexp(d)
   set.seed(1623L)
 
   ## rate = 0.5, rate_killing = 0, rate_drift = 0, jump_rate = 2
@@ -52,28 +44,25 @@ test_that("biv. CPP implementation works as intended for det. jumps", {
     "rjump_name" = "rposval", "rjump_arg_list" = list("value"=1),
     "barrier_values"=barrier_values)
   expect_equal_rn_generation(
-    "sample_cpp", "test__sample_cpp_R",
+    "sample_cpp", "test__sample_cpp",
     args, use_seed=use_seed)
 
   ## rate = 0.5, rate_killing = 0.2, rate_drift = 0, jump_rate = 2
   args[["rate_killing"]] <- 0.2
   expect_equal_rn_generation(
-    "sample_cpp", "test__sample_cpp_R",
+    "sample_cpp", "test__sample_cpp",
     args, use_seed=use_seed)
 
   ## rate = 0.5, rate_killing = 0.2, rate_drift = 0.1, jump_rate = 2
   args[["rate_drift"]] <- 0.1
   expect_equal_rn_generation(
-    "sample_cpp", "test__sample_cpp_R",
+    "sample_cpp", "test__sample_cpp",
     args, use_seed=use_seed)
 })
 
 
 ## #### Test implementation for the comonotone case ####
-#
-# Test that the implementation of the Compound Poisson process with
-# deterministic jumps works as expected for d = 2 and different
-# choices of parameters.
+
 test_that("CPP implementation works as indended for comonotone case", {
   set.seed(use_seed-543L)
   d <- 5L
@@ -85,9 +74,24 @@ test_that("CPP implementation works as indended for comonotone case", {
     "rjump_name" = "rposval", "rjump_arg_list" = list("value"=1),
     "barrier_values"=barrier_values)
   expect_equal_rn_generation(
-    "sample_cpp", "test__sample_cpp_R",
+    "sample_cpp", "test__sample_cpp",
     args, use_seed=use_seed)
 })
 
 
-## TODO: Implement tests for the independence case.
+## #### Test implementation for the independence case ####
+
+test_that("CPP implementation works as indended for independence case", {
+  set.seed(use_seed-543L)
+  d <- 5L
+  barrier_values <- rexp(2L)
+  set.seed(1623L)
+
+  args <- list(
+    "rate"=0, "rate_killing"=0, "rate_drift"=1,
+    "rjump_name" = "rposval", "rjump_arg_list" = list("value"=1),
+    "barrier_values"=barrier_values)
+  expect_equal_rn_generation(
+    "sample_cpp", "test__sample_cpp",
+    args, use_seed=use_seed)
+})
