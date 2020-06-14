53670a9c036e7b809e2c3c25285244e4ce288dfb
Henrik Sloot <henrik.sloot@gmail.com>
2020-06-15 00:19:14 +0200

    refactor-test: primitive Rcpp generator

--- tests/testthat/test__primitive_Rcpp_generators.R
+++ tests/testthat/test__primitive_Rcpp_generators.R
@@ -38,9 +38,13 @@ test_that("FixedDblGenerator works as expected", {
 
 test_that("RIntGenerator works as expected", {
   sample_int_base_0 <- function(n, probabilities) {
-    sapply(1:n, function(x) -1+sample.int(
-      n=length(probabilities),
-      size=1, prob = probabilities, replace=FALSE)
+    sapply(
+      1:n,
+      function(x) {
+        sample.int(
+          n=length(probabilities), size=1,
+          prob = probabilities, replace=FALSE) - 1
+      }
     )
   }
   probabilities <- c(8, 7, 3, 10, 6, 1, 2, 9, 5, 4)
@@ -71,8 +75,8 @@ test_that("RIntGenerator works as expected", {
 test_that("RSamplewalker works as expected", {
   perm_base_0 <- function(n, probabilities) {
     -1+sample.int(
-      n=length(probabilities),
-      size=n, prob = probabilities, replace=FALSE)
+      n=length(probabilities), size=n,
+      prob = probabilities, replace=FALSE)
   }
   probabilities <- c(8, 7, 3, 10, 6, 1, 2, 9, 5, 4)
   n <- length(probabilities)
@@ -105,101 +109,78 @@ test_that("RSamplewalker works as expected", {
 
 
 test_that("sample.int reimplementation works as expected", {
-  # TODO: This is very (!) repetitive and should be refactored with
-  # custom expectations.
-  # TODO: There should be a better way to produce meaningful
-  # parametrisations.
-  # TODO: These tests should be refactored in a seperate test file.
+  sample_int_base_0 <- function(
+      n=length(probabilities), size, prob = NULL, replace=FALSE) {
+    sample.int(n=n, size=size, prob = prob, replace=replace) - 1
+  }
+
+  args <- list("n" = 10, "size" = 10, "replace" = FALSE)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
+
+  args[c("n", "size", "replace")] <- c(1e2, 1e6, TRUE)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
+
+  args["prob"] <- list(seq(1:100))
+  args[c("n", "size", "replace")] <- c(100, 90, FALSE)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
+
+  args["prob"] <- list(seq(1:100) / sum(1:100))
+  args[c("n", "size", "replace")] <- c(100, 90, FALSE)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
+
+  args["prob"] <- list(seq(1:100))
+  args[c("n", "size", "replace")] <- c(100, 1e6, TRUE)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
+
+  args["prob"] <- list(seq(1:100) / sum(1:100))
+  args[c("n", "size", "replace")] <- c(100, 1e6, TRUE)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
 
   args <- list("n" = 10, "size" = 10, "replace" = FALSE)
   set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
 
   args[c("n", "size", "replace")] <- c(1e2, 1e6, TRUE)
   set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
 
   args["prob"] <- list(seq(1:100))
   args[c("n", "size", "replace")] <- c(100, 90, FALSE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
 
   args["prob"] <- list(seq(1:100) / sum(1:100))
   args[c("n", "size", "replace")] <- c(100, 90, FALSE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
 
   args["prob"] <- list(seq(1:100))
   args[c("n", "size", "replace")] <- c(100, 1e6, TRUE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
 
   args["prob"] <- list(seq(1:100) / sum(1:100))
   args[c("n", "size", "replace")] <- c(100, 1e6, TRUE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
-
-  args <- list("n" = 10, "size" = 10, "replace" = FALSE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
-
-  args[c("n", "size", "replace")] <- c(1e2, 1e6, TRUE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
-
-  args["prob"] <- list(seq(1:100))
-  args[c("n", "size", "replace")] <- c(100, 90, FALSE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
-
-  args["prob"] <- list(seq(1:100) / sum(1:100))
-  args[c("n", "size", "replace")] <- c(100, 90, FALSE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
-
-  args["prob"] <- list(seq(1:100))
-  args[c("n", "size", "replace")] <- c(100, 1e6, TRUE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
-
-  args["prob"] <- list(seq(1:100) / sum(1:100))
-  args[c("n", "size", "replace")] <- c(100, 1e6, TRUE)
-  set.seed(use_seed)
-  act <- do.call("Rcppmo_th_sample_int", args = args)
-  set.seed(use_seed)
-  exp <- do.call("sample.int", args = args)-1
-  expect_equal(act, exp)
+  expect_equal_rn_generation(
+    "Rcppmo_th_sample_int", "sample_int_base_0",
+    arg_list=args, use_seed=1623L)
 })
