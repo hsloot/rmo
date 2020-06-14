2b8b0a3b6427f354c9778ac5c75f1b39370ae9c8
Henrik Sloot <henrik.sloot@gmail.com>
2020-06-14 21:45:41 +0200

    refactor-test: Arnold model
    - fixes #29
    - delete bivariate implementation in `R`
    - enrich test implementation with comments
    - use new testcases for the Arnold model

--- tests/testthat/helper__arnold.R
+++ tests/testthat/helper__arnold.R
@@ -1,51 +1,31 @@
-#' Bivariate implementation of the Arnold model
-#'
-#' @rdname rmo_arnold
-#' @keywords internal
-#' @noRd
-test__rmo_arnold_bivariate_R <- function(n, d, intensities) { # nolint
-  total_intensity <- sum(intensities)
-  transition_probs <- intensities / total_intensity
-
-  out <- matrix(0, nrow=n, ncol=2)
-  for (i in 1:n) {
-  destroyed <- rep(FALSE, 2)
-    while (!all(destroyed)) {
-      waiting_time <- rexp(1, total_intensity)
-      affected <- sample.int(3, 1, replace=FALSE, prob=transition_probs)
-      out[i, !destroyed] <- out[i, !destroyed] + waiting_time
-      if (affected == 1) {
-        destroyed[1] <- TRUE
-      } else if (affected == 2) {
-        destroyed[2] <- TRUE
-      } else {
-        destroyed <- rep(TRUE, 2)
-      }
-    }
-  }
-
-  out
-}
-
-
 #' Original implementation of the Arnold model in `R`
 #'
 #' @rdname rmo_arnold
 #' @keywords internal
 #' @noRd
-test__rmo_arnold_R <- function(n, d, intensities) { # nolint
+test__rmo_arnold <- function(n, d, intensities) { # nolint
+  ## calculate total intensity as the sum of all shock
+  ## intensities and all transition probabilities as
+  ## the normalised shock intensities
   total_intensity <- sum(intensities)
   transition_probs <- intensities / total_intensity
 
   out <- matrix(nrow=n, ncol=d)
   for (k in 1:n) {
+    ## - set all entries of `destroyed` to `FALSE`, since all
+    ##   components are alive at the beginning
+    ## - repeat the following steps until all components are
+    ##   destroyed
     destroyed <- logical(d)
     value <- numeric(d)
-
     while (!all(destroyed)) {
+      ## sample waiting time and the arriving shock
       waiting_time <- rexp(1, total_intensity)
-      affected <- sample.int(n=2^d-1, size=1, replace=FALSE, prob=transition_probs)
-
+      affected <- sample.int(
+        n=2^d-1, size=1, replace=FALSE, prob=transition_probs)
+      ## check which components will be destroyed by the
+      ## current shock and increment waiting time for all
+      ## components which were alive before the shock arrival
       for (i in 1:d) {
         if (!destroyed[[i]]) {
           if (test__is_within(i, affected)) {
--- tests/testthat/test__arnold.R
+++ tests/testthat/test__arnold.R
@@ -3,81 +3,106 @@ use_seed <- 1632L
 n <- 100L
 
 ## #### Test implementation for the bivariate case ####
-#
-# Test that the implementation Arnold model works as expected the bivariate
-# case and different choices for the intensity vector.
 
 test_that("Arnold model implementation for d = 2", {
   ## all equal
-  args <- list("d" = 2L, "intensities" = c(1, 1, 1))
+  args <- list(
+    "d" = 2L,
+    "intensities" = c(1, 1, 1)
+  )
   expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_bivariate_R",
-    args, n, use_seed)
-
-  ## exchangeable, low, high
-  args <- list("d" = 2L, "intensities" = c(0.1, 0.1, 2))
-  expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_bivariate_R",
-    args, n, use_seed)
-
-  ## exchangeable, high, low
-  args <- list("d" = 2L, "intensities" = c(3, 3, 0.5))
-  expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_bivariate_R",
-    args, n, use_seed)
-
-  ## Low, High, Low
-  args[["intensities"]] <- c(0.5, 3, 0.2)
-  expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_bivariate_R",
-    args, n, use_seed)
-
-  ## Low, Low, High
-  args[["intensities"]] <- c(0.1, 0.005, 2)
-  expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_bivariate_R",
-    args, n, use_seed)
-
-  ## comonotone
-  args[["intensities"]] <- c(0, 0, 1)
-  expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_bivariate_R",
+    "rmo_arnold", "test__rmo_arnold",
     args, n, use_seed)
 
   ## independence
   args[["intensities"]] <- c(1, 1, 0)
   expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_bivariate_R",
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## comonotone
+  args[["intensities"]] <- c(0, 0, 1)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## exchangeable, low, high
+  args[["intensities"]] <- c(0.1, 0.1, 2)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## exchangeable, high, low
+  args[["intensities"]] <- c(3, 3, 0.5)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## Low, High, Low
+  args[["intensities"]] <- c(0.5, 3, 0.2)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## Low, Low, High
+  args[["intensities"]] <- c(0.1, 0.005, 2)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
     args, n, use_seed)
 })
 
 
+## ## test implementation against original `R` version ####
 
-## Test that the C++ implementation of the Arnold
-## model delivers the same result as the `R` implementation.
-test_that("Arnold model implementation in C++", {
-  # all equal
-  d <- 7L
-  intensities <- rep(0.5, times=2^d-1)
-  args <- list("d" = d, "intensities" = intensities)
+test_that("Arnold model implementation for d>2", {
+  ## dimension parameters
+  d1 <- 3L
+  d2 <- 4L
+  d <- d1 + d2
+
+  ## all equal
+  args <- list(
+    "d" = d,
+    "intensities" = rep(0.5, times=2^d-1)
+  )
   expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_R",
+    "rmo_arnold", "test__rmo_arnold",
     args, n, use_seed)
 
-  # d equals 4 and exchangeable
-  d <- 4L
-  intensities <- sapply(1:(2^d-1), function(x) {
-    cardinality <- sum(sapply(1:d, function(y) {
-      Rcpp__is_within(y, x)
-    }))
-
-    1 / cardinality
-  })
-  args <-list("d" = 4L, "intensities" = intensities)
+  ## independence
+  args[["intensities"]] <- intensities_constant(d, constant=0.7)
   expect_equal_rn_generation(
-    "rmo_arnold", "test__rmo_arnold_R",
+    "rmo_arnold", "test__rmo_arnold",
     args, n, use_seed)
 
-  ## TODO: Implement tests based on the parametrisation with Bernstein
-  ## functions to get realistic test-cases.
+  ## comonotone
+  args[["intensities"]] <- intensities_constant(d, constant=0.7)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## Poisson
+  args[["intensities"]] <- intensities_poisson(d, lambda=0.2, eta=0.3)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## Alpha-Stable
+  args[["intensities"]] <- intensities_alpha_stable(d, alpha=0.25)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## Gamma
+  args[["intensities"]] <- intensities_alpha_stable(d, a=0.4)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
+
+  ## Hierarchical
+  args[["intensities"]] <- intensities_hierarchical(
+    d1, d2, lambda=0.1, eta=0.3, a=0.2, alpha=0.4)
+  expect_equal_rn_generation(
+    "rmo_arnold", "test__rmo_arnold",
+    args, n, use_seed)
 })
