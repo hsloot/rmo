8c085ad928cbe7bb318fde37ebf8137b51856052
hsloot <16246049+hsloot@users.noreply.github.com>
2024-04-01 20:46:30 +0200

    fix(style): benchmarks

--- bench/poisson-15.R
+++ bench/poisson-15.R
@@ -3,6 +3,7 @@
 #' For the CPP LFM parametrization with *fixed size* jumps, we chose the
 #' parameters \( \lambda = 1 \) and \( \eta = 0.5 \).  This corresponds to a
 #' mean-jump-size of \( 0.5 \).
+
 #+ r parameters
 n <- 1e3
 d <- 15
@@ -18,11 +19,15 @@ ex_intensities <- rmo::exIntensities(bf, d)
 
 #+ r bench
 bench::mark(
-    Arnold = rmo:::Rcpp__rmo_am(
-        n, d, intensities = intensities),
-    ExMarkovian = rmo:::Rcpp__rexmo_mdcm(
-        n, d, ex_intensities = ex_intensities),
-    LFM = rmo:::Rcpp__rextmo_lfm(
-        n, d, 1, 0, 0, "rposval", list("value" = eta)),
-    min_iterations = 100L,
-    check = FALSE)
+  Arnold = rmo:::Rcpp__rmo_am(
+    n, d, intensities = intensities
+  ),
+  ExMarkovian = rmo:::Rcpp__rexmo_mdcm(
+    n, d, ex_intensities = ex_intensities
+  ),
+  LFM = rmo:::Rcpp__rextmo_lfm(
+    n, d, 1, 0, 0, "rposval", list("value" = eta)
+  ),
+  min_iterations = 100L,
+  check = FALSE
+)
