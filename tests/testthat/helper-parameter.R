660d6a893ce7fd6830381716f01768df39ef7e32
hsloot <16246049+hsloot@users.noreply.github.com>
2024-04-03 17:14:27 +0200

    depricate parameter generators

--- tests/testthat/helper-parameter.R
+++ tests/testthat/helper-parameter.R
@@ -262,3 +262,30 @@ intensities_inverse_gaussian <- function(d, eta) { # nocov start
   bf <- InverseGaussianBernsteinFunction(eta = eta)
   bf2intensities(d, bf)
 } # nocov end
+
+#' @keywords internal
+#' @noRd
+intensities_hierarchical <- function(d1, d2, lambda, eta, a, alpha) { # nolint # nocov start
+  ex_intensities_1 <- ex_intensities_gamma(d1, a)
+  ex_intensities_2 <- ex_intensities_alpha_stable(d2, alpha)
+
+  intensities <- lambda * intensities_poisson(d1 + d2, eta)
+  for (j in seq_along(intensities)) {
+    count_1 <- 0
+    count_2 <- 0
+    for (i in 1:d1) {
+      count_1 <- count_1 + Rcpp__is_within(i, j)
+    }
+    for (i in 1:d2) {
+      count_2 <- count_2 + Rcpp__is_within(d1 + i, j)
+    }
+
+    if (count_1 > 0 && count_2 == 0) {
+      intensities[j] <- intensities[j] + ex_intensities_1[count_1]
+    } else if (count_2 > 0 && count_1 == 0) {
+      intensities[j] <- intensities[j] + ex_intensities_2[count_2]
+    }
+  }
+
+  intensities
+} # nocov end
